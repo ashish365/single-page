a6bbd1176e6141f023e62150c3139fc6216a25e8
ashish365 <ashishraj365@gmail.com>
2020-06-18 13:32:52 +0530

    Updates

---
+++ precache-manifest.4e25b734a6956e4fabf59092ea13f1c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90da665276e1b6f9815fc8cc73dd0d5",
+    "url": "/single-page/index.html"
+  },
+  {
+    "revision": "97491c0083a18b2ff54a",
+    "url": "/single-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f4582eaaffbbf738ab79",
+    "url": "/single-page/static/js/2.284bd484.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/single-page/static/js/2.284bd484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97491c0083a18b2ff54a",
+    "url": "/single-page/static/js/main.e8c03f6d.chunk.js"
+  },
+  {
+    "revision": "6dbccb48d64c6bb426fc",
+    "url": "/single-page/static/js/runtime-main.9ec89d56.js"
+  }
+]);
