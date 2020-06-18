3bc09a305a302af8a9347f1c72a8e8d85659d56a
ashish365 <ashishraj365@gmail.com>
2020-06-18 14:59:23 +0530

    Updates

---
+++ precache-manifest.609fd9d7ec4796aa3ed7250d3c7bba06.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1d26ac201d9a45eec5b1f3adaaa79a",
+    "url": "/single-page/index.html"
+  },
+  {
+    "revision": "108503408a99d048ef8a",
+    "url": "/single-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d55f11a7f7f9610d804",
+    "url": "/single-page/static/js/2.671d8beb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/single-page/static/js/2.671d8beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108503408a99d048ef8a",
+    "url": "/single-page/static/js/main.f63671ee.chunk.js"
+  },
+  {
+    "revision": "6dbccb48d64c6bb426fc",
+    "url": "/single-page/static/js/runtime-main.9ec89d56.js"
+  },
+  {
+    "revision": "d346ba5b68b6211f5500e13492bc6b25",
+    "url": "/single-page/static/media/258872.d346ba5b.jpg"
+  },
+  {
+    "revision": "ed68be5098b23668db452da116b97340",
+    "url": "/single-page/static/media/iitg1.ed68be50.jpg"
+  },
+  {
+    "revision": "ac4e777224fcafebf03b2bdfac4a1bee",
+    "url": "/single-page/static/media/lionel-messi.ac4e7772.jpg"
+  },
+  {
+    "revision": "0610db91e50113aee912a28ed88800b4",
+    "url": "/single-page/static/media/ronaldo.0610db91.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/single-page/precache-manifest.5113cf75c8644fdf90a66f499a21b035.js"
+  "/single-page/precache-manifest.609fd9d7ec4796aa3ed7250d3c7bba06.js"
 );
 
 self.addEventListener('message', (event) => {
